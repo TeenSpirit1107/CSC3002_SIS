78f27b6e79babb05b4f4ee77a79f13e5f4e3f964
igttttma <149131713+igttttma@users.noreply.github.com>
2024-11-13 20:53:13 +0800

    Add files via upload

--- sis_ws/include/registry.hpp
+++ sis_ws/include/registry.hpp
@@ -7,7 +7,22 @@
 // packages
 #include "client.hpp"
 using namespace std;
-
+class Announcement: public Client {
+protected:
+    int textLineCount;
+    string title;
+    string text;
+    string promoter;
+    string targetUser;
+    string promoteTime;
+public:
+    Announcement();
+    void setContent(string title, string text);
+    void setPromoter(string promoter);
+    void setTarget(string idCode);
+    void send();
+    ~Announcement();
+};
 class Registry: public Client{
 
 protected:
