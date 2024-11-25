fb8bd9468a099b4f927afe698279426e4c60ccef
Linyong GAN <123090120@link.cuhk.edu.cn>
2024-11-25 20:37:06 +0800

    1125 reg v3 grade related. All the functions of registry except for d and h are all finished.

---
+++ cmake-build-debug/CMakeFiles/CSC3002_SIS.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "E:/blackboard/CSC3002CC++ Programming_L01L02/project/CSC3002_SIS/sis_ws/src/client.cpp" "CMakeFiles/CSC3002_SIS.dir/sis_ws/src/client.cpp.obj" "gcc" "CMakeFiles/CSC3002_SIS.dir/sis_ws/src/client.cpp.obj.d"
+  "E:/blackboard/CSC3002CC++ Programming_L01L02/project/CSC3002_SIS/sis_ws/src/course.cpp" "CMakeFiles/CSC3002_SIS.dir/sis_ws/src/course.cpp.obj" "gcc" "CMakeFiles/CSC3002_SIS.dir/sis_ws/src/course.cpp.obj.d"
+  "E:/blackboard/CSC3002CC++ Programming_L01L02/project/CSC3002_SIS/sis_ws/src/registry.cpp" "CMakeFiles/CSC3002_SIS.dir/sis_ws/src/registry.cpp.obj" "gcc" "CMakeFiles/CSC3002_SIS.dir/sis_ws/src/registry.cpp.obj.d"
+  "E:/blackboard/CSC3002CC++ Programming_L01L02/project/CSC3002_SIS/sis_ws/src/registryTest.cpp" "CMakeFiles/CSC3002_SIS.dir/sis_ws/src/registryTest.cpp.obj" "gcc" "CMakeFiles/CSC3002_SIS.dir/sis_ws/src/registryTest.cpp.obj.d"
+  "E:/blackboard/CSC3002CC++ Programming_L01L02/project/CSC3002_SIS/sis_ws/src/staff.cpp" "CMakeFiles/CSC3002_SIS.dir/sis_ws/src/staff.cpp.obj" "gcc" "CMakeFiles/CSC3002_SIS.dir/sis_ws/src/staff.cpp.obj.d"
+  "E:/blackboard/CSC3002CC++ Programming_L01L02/project/CSC3002_SIS/sis_ws/src/student.cpp" "CMakeFiles/CSC3002_SIS.dir/sis_ws/src/student.cpp.obj" "gcc" "CMakeFiles/CSC3002_SIS.dir/sis_ws/src/student.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/CSC3002_SIS.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CSC3002_SIS.
+# This may be replaced when dependencies are built.
